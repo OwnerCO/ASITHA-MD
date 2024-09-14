1a2a024fb41880bd6ec55afd8fae1d39a85c30ea
OwnerCO <jazminqntro96@gmail.com>
2024-09-14 18:32:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "l3lBXT5C#ewgsWiRiy6p475xNt6mCvf1Kx03rRNTHTlv27sqsmA4",
+MONGODB: process.env.MONGODB || "mongodb+srv://ownerco:ownerco@gifted.h5i4ndp.mongodb.net/?retryWrites=true&w=majority",//enter mongo db url
 };
